a1461feefb26e106fd77a0dc6224d072d793b66c
merlin <highboi@protonmail.com>
2020-10-15 21:17:21 -0500

    Final POC for OBS streaming using Node.js

---
+++ app.js
@@ -0,0 +1,38 @@
+const NodeMediaServer = require("node-media-server");
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const path = require("path");
+
+
+app.use(express.static(path.join(__dirname, "views")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const config = {
+	rtmp: {
+		port: 1935,
+		chunk_size: 60000,
+		gop_cache: true,
+		ping: 30,
+		ping_timeout: 60
+	},
+	http: {
+		port: 8000,
+		mediaroot: "./media",
+		allow_origin: "*"
+	}
+};
+
+var nms = new NodeMediaServer(config);
+nms.run();
+
+
+app.get("/", (req, res) => {
+	res.render("index.ejs");
+});
+
+app.listen(777, '0.0.0.0', () => {
+	console.log("server running");
+});
+
